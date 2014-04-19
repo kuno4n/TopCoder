a66139e544ce43f73cae2e185a6f6ddb8debeb67
kuno4n <kuno4n@gmail.com>
2014-04-19 18:18:50 +0900

    20140419

--- common/cout.h
+++ common/cout.h
@@ -47,7 +47,7 @@ template <typename T> ostream& operator<<(ostream &s, list<T> ls)
 {
     int cnt = 0;
     s << "( ";
-    for (typeof(ls.begin()) it=ls.begin(); it!=ls.end(); it++) {
+    for (auto it=ls.begin(); it!=ls.end(); it++) {
         if (it != ls.begin()) s << ", ";
         s << *it;
         cnt++;
@@ -59,7 +59,7 @@ template <typename T> ostream& operator<<(ostream &s, deque<T> st)
 {
     int cnt = st.size();
     s << "[ ";
-    for (typeof(st.begin()) it=st.begin(); it!=st.end(); it++) {
+    for (auto it=st.begin(); it!=st.end(); it++) {
         if (it != st.begin()) s << ", ";
         s << *it;
     }
@@ -70,7 +70,7 @@ template <typename T1, typename T2> ostream& operator<<(ostream &s, map<T1,T2> m
 {
     int cnt = m.size();
     s << "{ ";
-    for (typeof(m.begin()) it=m.begin(); it!=m.end(); it++) {
+    for (auto it=m.begin(); it!=m.end(); it++) {
         if (it != m.begin()) s << ", ";
         s << it->first << " => " << it->second;
     }
@@ -81,7 +81,7 @@ template <typename T> ostream& operator<<(ostream &s, set<T> st)
 {
     int cnt = st.size();
     s << "[ ";
-    for (typeof(st.begin()) it=st.begin(); it!=st.end(); it++) {
+    for (auto it=st.begin(); it!=st.end(); it++) {
         if (it != st.begin()) s << ", ";
         s << *it;
     }
