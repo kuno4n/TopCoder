1c7ac4a093d417676c5520e21fe4b77be392be9d
kuno4n <kuno4n@gmail.com>
2015-01-23 16:53:57 +0900

    20150123

---
+++ src/610-div1-500/AlbertoTheAviator.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <cstdarg>
+#include <sys/time.h>
+#include <fstream>
+//#include "cout.h"
+
+using namespace std;
+
+#define SZ(x) ((int)x.size())
+#define MSET(x,a) memset(x, a, (int)sizeof(x))
+#define PB push_back
+#define VI vector < int >
+#define PII pair < int, int >
+#define LL long long
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(), (v).end()
+#define FIT(it,v) for (typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+#define OUT(A) cout << #A << " = "<< (A) << endl
+#define OUT2(A, B) cout << "(" << #A << ", " << #B << ") = (" << (A) << ", "<< (B) << ")" << endl
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } 
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } 
+
+int F;
+vector<int> duration;
+vector<int> refuel;
+
+int dp[55][55];
+
+class AlbertoTheAviator {
+public:
+    int MaximumFlights(int _F, vector<int> _duration, vector<int> _refuel) {
+        F = _F, duration = _duration, refuel = _refuel;
+        vector<pair<int, int> > vi;
+        REP(i, SZ(duration)) vi.push_back(make_pair(refuel[i], duration[i]));
+        sort(ALL(vi));
+        reverse(ALL(vi));
+        MSET(dp, -1);
+        dp[0][0] = F;
+        if(vi[0].second <= F) dp[0][1] = F-vi[0].second+vi[0].first;
+        FOR(i, 1, SZ(vi)){
+            REP(j, 55){
+                dp[i][j] = dp[i-1][j];
+                if(j > 0 && dp[i-1][j-1] >= vi[i].second) chmax(dp[i][j] , dp[i-1][j-1] - vi[i].second + vi[i].first);
+            }
+        }
+        int res = 0;
+        REP(i, 55) if(dp[SZ(vi)-1][i] >= 0) res = i;
+        return res;
+    }
+};
+
+// CUT begin
+ifstream data("AlbertoTheAviator.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int F, vector<int> duration, vector<int> refuel, int __expected) {
+    time_t startClock = clock();
+    AlbertoTheAviator *instance = new AlbertoTheAviator();
+    int __result = instance->MaximumFlights(F, duration, refuel);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int F;
+        from_stream(F);
+        vector<int> duration;
+        from_stream(duration);
+        vector<int> refuel;
+        from_stream(refuel);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(F, duration, refuel, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1421995243;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 550 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "AlbertoTheAviator (550 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
---
+++ src/610-div2-1000/MiningGoldEasy.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <cstdarg>
+#include <sys/time.h>
+#include <fstream>
+//#include "cout.h"
+
+using namespace std;
+
+#define SZ(x) ((int)x.size())
+#define MSET(x,a) memset(x, a, (int)sizeof(x))
+#define PB push_back
+#define VI vector < int >
+#define PII pair < int, int >
+#define LL long long
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(), (v).end()
+#define FIT(it,v) for (typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+#define OUT(A) cout << #A << " = "<< (A) << endl
+#define OUT2(A, B) cout << "(" << #A << ", " << #B << ") = (" << (A) << ", "<< (B) << ")" << endl
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } 
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } 
+
+int N;
+int M;
+vector<int> event_i;
+vector<int> event_j;
+int n;
+
+int memo[55][55][55];
+
+int rec(int x, int y, int t){
+    int &res = memo[x][y][t];
+    if(res >= 0) return res;
+    if(t == n) return res = 0;
+    int a = event_i[x];
+    int b = event_j[y];
+    int tmp = N + M - abs(event_i[t] - a) - abs(event_j[t] - b);
+    REP(i, n) chmax(res, tmp + rec(i, y, t+1));
+    REP(j, n) chmax(res, tmp + rec(x, j, t+1));
+    return res;
+}
+
+class MiningGoldEasy {
+public:
+    int GetMaximumGold(int _N, int _M, vector<int> _event_i, vector<int> _event_j) {
+        N = _N, M = _M, event_i = _event_i, event_j = _event_j;
+        n = SZ(event_i);
+        int res = 0;
+        MSET(memo, -1);
+        REP(i, n){
+            REP(j, n){
+                chmax(res, rec(i, j, 0));
+            }
+        }
+        return res;
+    }
+};
+
+// CUT begin
+ifstream data("MiningGoldEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, int M, vector<int> event_i, vector<int> event_j, int __expected) {
+    time_t startClock = clock();
+    MiningGoldEasy *instance = new MiningGoldEasy();
+    int __result = instance->GetMaximumGold(N, M, event_i, event_j);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        int M;
+        from_stream(M);
+        vector<int> event_i;
+        from_stream(event_i);
+        vector<int> event_j;
+        from_stream(event_j);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, M, event_i, event_j, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1421990255;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MiningGoldEasy (1000 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
